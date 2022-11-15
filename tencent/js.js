ddb7ae5b93d72f8f14b9e3027e3aeb7d775ed96d
dayflywu <dayflywu@tencent.com>
2022-11-15 13:24:14 +0800

    fix: 修改强制文件末尾的最大连续空行数为1

--- tencent/js.js
+++ tencent/js.js
@@ -394,7 +394,7 @@ module.exports = warnToError({
      */
     'no-multiple-empty-lines': [
       'error',
-      { max: 1, maxEOF: 0 },
+      { max: 1, maxEOF: 1 },
     ],
     /**
      * 禁止使用嵌套的三元表达式，比如 a ? b : c ? d : e
